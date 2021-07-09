cb742ccc3561013a3aeb291f978cdafe75231740
Aubri Henley <aubri.henley@gmail.com>
2021-07-09 13:59:34 -0400

    added current date and time

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//setting todays date and time
+function setTime() {
+    // Sets interval in variable
+    setInterval(function() {
+        var unix;
+        $().text(unix);
+        
+        var unixFormat = moment(unix).format("dddd, MMMM Do YYYY, h:mm:ss a");
+        $("#currentDay").text(unixFormat);
+      }, 1000);
+    }
+    setTime();
