2fb2c1a6e20a95c30352af4ec27b6f981f13c31f
Aubri Henley <aubri.henley@gmail.com>
2021-07-13 14:05:23 -0400

    missed save in index.html

--- assets/script.js
+++ assets/script.js
@@ -15,12 +15,16 @@ function setTime() {
   //change color in the html add/remove classes in css
   function displayColors() {
 
-    var currentTime = moment(unix).format("H");
-  
+    var currentTime = moment().format("H");
+    var taskTime = element.getAttribute("data-time")
+
+    if (currentTime === taskTime ){
+      
+    }
 
   }
 
   // Loop through all h4 elements to add styling 
 for (var i = 0; i < input.length; i++) {
-  siteTitles[i].setAttribute("style", "color: blue; font-weight: bolder; font-size: 30px; padding-left:10px; margin: 0");
+
 }
