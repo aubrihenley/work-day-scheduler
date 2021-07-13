c03088e6096f177af7a3a4c78b6a896f43d1124c
Aubri Henley <aubri.henley@gmail.com>
2021-07-13 13:45:57 -0400

    added data-time to timeblock rows

--- assets/script.js
+++ assets/script.js
@@ -17,5 +17,10 @@ function setTime() {
 
     var currentTime = moment(unix).format("H");
   
-  console.log(currentTime);
+
   }
+
+  // Loop through all h4 elements to add styling 
+for (var i = 0; i < input.length; i++) {
+  siteTitles[i].setAttribute("style", "color: blue; font-weight: bolder; font-size: 30px; padding-left:10px; margin: 0");
+}
